03166af148822a4f8d7cb159f43299678ea4117b
akorshun <razor1702@gmail.com>
2018-04-12 12:12:48 +0300

    Update aokp.mk

--- aokp.mk
+++ aokp.mk
@@ -1,2 +1,2 @@
-$(call inherit-product, vendor/aokp/config/common_full_phone.mk)
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 $(call inherit-product, device/lineage/sepolicy/common/sepolicy.mk)
